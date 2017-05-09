bedaaa7c3238028381a571dab39325faaad94e99
Hoàng Minh Nhật <nhathm01247@gmail.com>
2017-05-09 11:36:34 +0700

    Push sample source

---
+++ SampleCustomizeSectionHeader/SampleCustomizeSectionHeader/HeaderView.swift
@@ -0,0 +1,23 @@
+//
+//  HeaderView.swift
+//  SampleCustomizeSectionHeader
+//
+//  Created by RTC-HN158 on 5/9/17.
+//  Copyright © 2017 NhatHM. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UITableViewHeaderFooterView {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var year: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
